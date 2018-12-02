5bb3d2834c47631760b928f4bd330ca912a638bd
github username <github email address>
2018-12-02 12:08:31 +0000

    Added Footer component

--- src/app/index.js
+++ src/app/index.js
@@ -3,14 +3,17 @@ import { Route } from "react-router-dom";
 
 import "./App.css";
 import Home from "../containers/home";
-import Header from "../components/header"
+import Header from "../components/header";
+import Footer from "../components/footer";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
         <Route exact path="/" render={Home} />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/footer/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Footer= () => <p>Footer</p>
+
+
+export default Footer
