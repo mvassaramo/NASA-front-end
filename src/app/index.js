0fa7ae7928d16faddbaacd6d56c9b717373a240e
github username <github email address>
2018-12-07 17:12:01 +0000

    updated redirect to gallery page router

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,67 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
 import "./App.css";
 import Home from "../containers/home";
 import Gallery from "../containers/gallery";
-
 import Header from "../components/header";
 import Footer from "../components/footer";
+import AssetDetails from "../components/assetDetails";
+import Search from "../components/search";
+
+const API = "https://images-api.nasa.gov";
 
 class App extends Component {
+  state = {
+    searchQuery: undefined,
+    assets: [],
+    search: false
+  };
+
+  enterPressed = event => {
+    const code = event.keyCode || event.which;
+    if (code === 13) {
+      this.getAssets();
+    } else return null;
+  };
+
+  updateSearchQuery = searchQuery => this.setState({ searchQuery });
+
+  getAssets = () => {
+    return fetch(`${API}/search?q=${this.state.searchQuery}`)
+      .then(res => res.json())
+      .then(data =>
+        this.setState({ assets: data["collection"]["items"], search: true })
+      );
+  };
+
   render() {
+    const { assets, search } = this.state;
     return (
       <div className="App">
         <Header />
-        <Route exact path="/" render={props => <Home {...props} />} />
-        <Route exact path="/gallery" render={props => <Gallery {...props} />} />
+        <Search
+          updateSearchQuery={this.updateSearchQuery}
+          getAssets={this.getAssets}
+          enterPressed={this.enterPressed}
+        />
+        <Route
+          exact
+          path="/"
+          render={props =>
+            search ? <Redirect to="/gallery" {...props} /> : <Home {...props} />
+          }
+        />
+        <Route
+          exact
+          path="/gallery"
+          render={props => <Gallery {...props} assets={assets} />}
+        />
+        <Route
+          exact
+          path="/gallery/:nasa_id"
+          render={props => <AssetDetails {...props} assets={assets} />}
+        />
         <Footer />
       </div>
     );
--- src/containers/gallery/index.js
+++ src/containers/gallery/index.js
@@ -1,50 +1,24 @@
 import React from "react";
 
-import Search from "../../components/search";
 import Asset from "../../components/asset";
 
-
-const API = "https://images-api.nasa.gov";
-
 class Gallery extends React.Component {
-  state = {
-    searchQuery: undefined,
-    assets: []
-  };
-
-  enterPressed = (event) => {
-    const code = event.keyCode || event.which 
-    return code === 13 ? this.getAssets() : null 
-  }
-
-  updateSearchQuery = searchQuery => this.setState({ searchQuery });
-
-  getAssets = () => {
-    return fetch(`${API}/search?q=${this.state.searchQuery}`)
-      .then(res => res.json())
-      .then(data => this.setState({ assets: data["collection"]["items"] }));
-  };
-
   render() {
     return (
-      <React.Fragment><br></br>
-        <Search
-          updateSearchQuery={this.updateSearchQuery}
-          getAssets={this.getAssets}
-          enterPressed={this.enterPressed}
-        />
+      <React.Fragment>
+        <br />
+
         <div className="gallery-container">
-          {this.state.assets.length > 0 ? (
-            this.state.assets.map(asset =>
+          {this.props.assets.length > 0 ? (
+            this.props.assets.map(asset =>
               asset.data[0].media_type === "image" ? (
-                <Asset asset={asset} /> 
+                <Asset asset={asset} />
               ) : null
             )
           ) : (
             <p>no results found</p>
           )}
         </div>
-
       </React.Fragment>
     );
   }
