7ae3f58279ded9345ec6284a0022d972679eef34
github username <maduri.vassaramo@gmail.com>
2018-12-07 19:52:15 +0000

    added back button to assetDetail component

--- src/components/assetDetails/index.js
+++ src/components/assetDetails/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 
+
 import "./assetDetails.css"
 
 const AssetDetails = props => {
+    console.log(props)
      const {title, description} = props.asset.data[0]
   return (props.asset ? 
     <div className="assetDetail-container">
       <p>{title}</p>
       <p>{description}</p>
       <img src={props.asset.links[0].href} alt={title} />
+      <button onClick={props.history.goBack}>Take me back</button>
     </div>
    : null);
 };
