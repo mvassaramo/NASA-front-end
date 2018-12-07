cd0fab9927f30b2db19a810b1f4f9c92ce5b6a8d
github username <maduri.vassaramo@gmail.com>
2018-12-07 19:19:21 +0000

    updated gallery page asset cards and assetDetails styling

--- src/components/assetDetails/index.js
+++ src/components/assetDetails/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import "./assetDetails.css"
+
 const AssetDetails = props => {
      const {title, description} = props.asset.data[0]
-  return props.asset ? (
-    <div>
+  return (props.asset ? 
+    <div className="assetDetail-container">
       <p>{title}</p>
       <p>{description}</p>
       <img src={props.asset.links[0].href} alt={title} />
     </div>
-  ) : null;
+   : null);
 };
 
 export default AssetDetails;
